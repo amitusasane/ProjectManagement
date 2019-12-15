a666e68333c3f9b8821af4922883ffa6ee29d2da
amitusasane <amitusasane@gmail.com>
2019-12-14 18:04:30 -0600

    Added unit test case for Add User

--- frontend/src/components/AddUser/AddUser.test.js
+++ frontend/src/components/AddUser/AddUser.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
-import { render, cleanup, wait } from '@testing-library/react';
+import { render, cleanup, wait, fireEvent } from '@testing-library/react';
+import { toBeDisabled, toBeEmpty } from '@testing-library/jest-dom';
+expect.extend({ toBeDisabled, toBeEmpty });
+
 import AddUser from './AddUser';
+import axios from 'axios';
+jest.mock('axios');
 
 describe('Add User Component', () => {
+  const onSubmit = jest.fn();
+  const fakeFirstName = 'SHREE User01';
+  const fakeLastName = 'user last name';
+  const fakeEmpId = '11';
+
+  let userList = {
+    data: [{ _id: '5df53177f576f77b78e0e6d9', firstName: 'User01', lastName: '01Last', empId: 1 }]
+  };
+  beforeEach(() => {
+    axios.get.mockResolvedValue(userList);
+  });
   afterEach(cleanup);
   it('Should render Add User component', async () => {
+    //axios.get.mockResolvedValue(userList);
     const { container } = render(
       <Router>
         <AddUser />
@@ -15,7 +32,86 @@ describe('Add User Component', () => {
       expect(container).toMatchSnapshot();
     });
   });
-  // it('Should render Add User component', () => {
-  //   expect(true).toBe(true);
-  // });
+  it('Get user List ', async () => {
+    //axios.get.mockResolvedValue(userList);
+    const { container, getByText } = render(
+      <Router>
+        <AddUser />
+      </Router>
+    );
+    expect(getByText('Users are not available in database!!'));
+  });
+
+  it('Should able to Add User ', async () => {
+    //axios.get.mockResolvedValue(userList);
+
+    const { container, getByText, getByPlaceholderText } = render(
+      <Router>
+        <AddUser />
+      </Router>
+    );
+    const firstName = getByPlaceholderText('FirstName');
+    const lastName = getByPlaceholderText('LastName');
+    const empId = getByPlaceholderText('Employee ID');
+    const submitBtn = getByText('Add');
+
+    fireEvent.change(firstName, { target: { name: 'firstName', value: fakeFirstName } });
+    fireEvent.change(lastName, { target: { name: 'lastName', value: fakeLastName } });
+    fireEvent.change(empId, { target: { name: 'empId', value: fakeEmpId } });
+
+    await wait(() => {
+      expect(submitBtn).not.toBeDisabled();
+      fireEvent.click(submitBtn);
+    });
+  });
+
+  it('Should able to Edit User ', async () => {
+    const { container, getByText, getByPlaceholderText } = render(
+      <Router>
+        <AddUser />
+      </Router>
+    );
+    await wait(() => {
+      const firstName = getByPlaceholderText('FirstName');
+      const editBtn = getByText('Edit');
+
+      fireEvent.click(editBtn);
+
+      const updateBtn = getByText('Update');
+      fireEvent.change(firstName, { target: { name: 'firstName', value: fakeFirstName } });
+
+      expect(updateBtn).not.toBeDisabled();
+      fireEvent.click(updateBtn);
+    });
+    //axios.post.mockResolvedValue({ message: 'User updated successfully' });
+    //expect(getByText('User updated successfully'));
+  });
+
+  it('Should able to Delete User ', async () => {
+    const { container, getByText, getByPlaceholderText } = render(
+      <Router>
+        <AddUser />
+      </Router>
+    );
+    await wait(() => {
+      const DeleteBtn = getByText('Delete');
+      expect(DeleteBtn).not.toBeDisabled();
+      fireEvent.click(DeleteBtn);
+      //expect(getByText('User deleted successfully'));
+    });
+  });
+
+  it('Should able to Reset User Form ', async () => {
+    const { container, getByText, getByPlaceholderText } = render(
+      <Router>
+        <AddUser />
+      </Router>
+    );
+    const firstName = getByPlaceholderText('FirstName');
+    fireEvent.change(firstName, { target: { name: 'firstName', value: fakeFirstName } });
+    const resetBtn = getByText('Reset');
+    expect(resetBtn).not.toBeDisabled();
+    fireEvent.click(resetBtn);
+    expect(firstName).toBeEmpty();
+  });
 });
